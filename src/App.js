00856e1c32ab34720b52208b63b0180bd87dd0a8
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2021-12-21 19:25:45 -0600

    Adding resources and index file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createElement } from "react";
+import Header from "./components/base/Header";
+import './styles/App.css';
+import Navbar, { NavListItems } from "./components/base/Navbar";
+import CircleImageContainer from "./components/CircleImage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import image from './res/assets/profile-picture.jpg';
+import github from './res/assets/github_icon_128.png'
+import facebook from './res/assets/facebook_icon_128.png'
+import linkedin from './res/assets/linkedin_icon_128.png'
+
+const navMenuItems = [
+    { properties: { className: "list-item", key:"home" }, children: "Home" },
+    { properties: { className: "list-item", key:"aboutme" }, children: "About me" },
+    { properties: { className: "list-item", key:"whatido" }, children: "What I Do" },
+    { properties: { className: "list-item", key:"portfolio" }, children: "Portfolio" }
+]
+
+
+const socialNetworks = [
+    { title: "Github", properties: { imgSrc: github, className: "icon" } },
+    { title: "Linkedin", properties: { imgSrc: linkedin, className: "icon" } },
+    { title: "Facebook", properties: { imgSrc: facebook, className: "icon" } },
+]
+
+const App = () => {
+    return (
+
+        <div>
+            <Header className={"header"}>
+
+                <Navbar className={"navbar"}>
+
+                    <div>
+                        <h1 id={"domain-name"}>{"Soybrandon"}</h1>
+                    </div>
+
+                    <div>
+                        <NavListItems className={"nav-list"} elements={navMenuItems} />
+                    </div>
+
+                </Navbar>
+
+                <div id={"profile-container"}>
+
+                    <div id={"social-network-container"}>
+                        {
+                            socialNetworks.map(e => {
+                                return createElement(CircleImageContainer, e.properties)
+                            })
+                        }
+                    </div>
+                    <CircleImageContainer
+                        className={"profile-picture"}
+                        imgSrc={image}
+                        size={{ w: 250, h: 250 }}
+                    />
+
+                    <h2 id={"profile-name"}>{"Brandon Isaac Fonseca"}</h2>
+                </div>
+
+            </Header>
+
+            <main>
+                
+            </main>
+        </div>
+    );
 }
 
 export default App;
