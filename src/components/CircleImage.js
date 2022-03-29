265568486689f92f3c98d8a46c61af0d6ad602de
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2022-03-28 22:25:42 -0600

    Adding Circle Filled components with bouncing animations

--- src/components/CircleImage.js
+++ src/components/CircleImage.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import "./styles/CircleImage.css"
 const CircleImageContainer = ({ className, imgSrc, alt, toolTip, size = { w: "45px", h: "45px" } }) => {
 
     const { w, h } = size;
@@ -17,4 +18,25 @@ const CircleImageContainer = ({ className, imgSrc, alt, toolTip, size = { w: "45
 
 }
 
+export const CircleFilled = ({ shadowed = false, sh_animations, animations, title = "Title", color = "darkgrey" }) => {
+    return (
+        <div className={"tech-circle"}>
+
+            <div
+                id="filled-circle"
+                className={`soft-shader ${animations}`}
+                style={{ backgroundColor: color }}>
+
+                <h3>{title}</h3>
+
+            </div>{applyShadow(shadowed, sh_animations)}</div>
+    )
+}
+
+
+function applyShadow(isShadowed, sh_animations) {
+    if (isShadowed)
+        return <div id="shadow" className={`${sh_animations}`}></div>
+
+}
 export default CircleImageContainer;
